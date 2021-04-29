b45664b39d82e80abd101277cd0f9b5bc03defad
Jack Chen <redchenjs@live.com>
2021-04-29 23:21:27 +0800

    remove lookup tables

--- update.sh
+++ update.sh
@@ -10,7 +10,11 @@ for dir in ExtraLight Light Regular Medium SemiBold Bold Heavy; do
   sed "s|SerifK|SerifC|" "source-han-serif/$dir/OTC/cidfont.ps.OTC.K" > "$dir/OTC/cidfont.ps.OTC.CL"
   sed "s|SerifK|SerifC|
        s|Korean|Classic|" "source-han-serif/$dir/OTC/cidfontinfo.OTC.K" > "$dir/OTC/cidfontinfo.OTC.CL"
-  search_and_delete 'feature locl' 'locl;\n' "$dir/OTC/features.OTC.CL"
+  sed -i "/lookup kr2.*;$/d" "$dir/OTC/features.OTC.CL"
+  search_and_delete 'lookup kr2jp' 'kr2jp;\n' "$dir/OTC/features.OTC.CL"
+  search_and_delete 'lookup kr2cn' 'kr2cn;\n' "$dir/OTC/features.OTC.CL"
+  search_and_delete 'lookup kr2tw' 'kr2tw;\n' "$dir/OTC/features.OTC.CL"
+  search_and_delete 'lookup kr2hk' 'kr2hk;\n' "$dir/OTC/features.OTC.CL"
 done
 
 sed "s|SerifKR|SerifCL|" "source-han-serif/UniSourceHanSerifKR-UTF32-H" > UniSourceHanSerifCL-UTF32-H
