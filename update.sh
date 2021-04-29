0a52ec989b726a3d3e91cf091c57aa3a5f633438
Jack Chen <redchenjs@live.com>
2021-04-29 22:53:16 +0800

    remove locl feature

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+search_and_delete() {
+  sed -i ":begin; /$1/,/$2/ { /$2/! { \$! { N; b begin }; }; /$1.*$2/d; };" $3
+}
+
 for dir in ExtraLight Light Regular Medium SemiBold Bold Heavy; do
   mkdir -p "$dir/OTC"
   sed "s|SerifK|SerifC|" "source-han-serif/$dir/OTC/features.OTC.K" > "$dir/OTC/features.OTC.CL"
   sed "s|SerifK|SerifC|" "source-han-serif/$dir/OTC/cidfont.ps.OTC.K" > "$dir/OTC/cidfont.ps.OTC.CL"
   sed "s|SerifK|SerifC|
        s|Korean|Classic|" "source-han-serif/$dir/OTC/cidfontinfo.OTC.K" > "$dir/OTC/cidfontinfo.OTC.CL"
+  search_and_delete 'feature locl' 'locl;\n' "$dir/OTC/features.OTC.CL"
 done
 
 sed "s|SerifKR|SerifCL|" "source-han-serif/UniSourceHanSerifKR-UTF32-H" > UniSourceHanSerifCL-UTF32-H
