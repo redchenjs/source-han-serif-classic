b7853f6b63c9844d2ce525c4595afbe7094d87d9
Jack Chen <redchenjs@live.com>
2021-04-30 18:51:38 +0800

    update build scripts

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+variant=Serif
+subdirs='ExtraLight Light Regular Medium SemiBold Bold Heavy'
 
 mkdir -p OTF
 
-for dir in ExtraLight Light Regular Medium SemiBold Bold Heavy; do
+for dir in $subdirs; do
   cd "$dir/OTC"
-  makeotf -f cidfont.ps.OTC.CL -omitMacNames -ff features.OTC.CL -fi cidfontinfo.OTC.CL -mf ../../FontMenuNameDB -r -nS -cs 3 -ch ../../UniSourceHanSerifCL-UTF32-H -ci ../../SourceHanSerif_CL_sequences.txt ; tx -cff +S cidfont.ps.OTC.CL CFF.OTC.CL ; sfntedit -a CFF=CFF.OTC.CL "SourceHanSerifC-$dir.otf"
-  mv "SourceHanSerifC-$dir.otf" ../../OTF
+  makeotf -f cidfont.ps.OTC.CL -omitMacNames -ff features.OTC.CL -fi cidfontinfo.OTC.CL -mf ../../FontMenuNameDB -r -nS -cs 3 -ch "../../UniSourceHan${variant}CL-UTF32-H" -ci "../../SourceHan${variant}_CL_sequences.txt" ; tx -cff +S cidfont.ps.OTC.CL CFF.OTC.CL ; sfntedit -a CFF=CFF.OTC.CL "SourceHan${variant}C-$dir.otf"
+  mv "SourceHan${variant}C-$dir.otf" ../../OTF
   rm CFF.OTC.CL
   cd ../../
 done
 
-zip source-han-serif-classic-otf.zip LICENSE.txt OTF/*
+zip "SourceHan${variant}C-OTF.zip" LICENSE.txt OTF/*
+sha512sum "SourceHan${variant}C-OTF.zip" | sed -r 's|([0-9a-z]+).*|\1|g' > "SourceHan${variant}C-OTF.zip.sha512sum"
